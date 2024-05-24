1a5e1fd1f7f76dec29a55fc060e7eeece0f569bf
Mehedi hasan ronm <hassan.mehedi2026@gmail.com>
2024-05-24 06:10:05 +0600

    added jwt

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var jwt = require('jsonwebtoken');
 
 var cookieParser = require('cookie-parser')
 const app = express()
-const port = 5000
+
+const port = process.env.PORT || 5000;
 
 const secreckey = "hello there"
 
